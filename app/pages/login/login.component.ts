9e91d4e237434274bb86cbe41140796e7da78d1e
Chisomo Sakala <chisomo.sakala@mombo.solutions>
2016-07-20 02:28:34 -0500

    Finished step 4: NaitiveScript modules

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,7 +1,11 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {User} from "../../shared/user/user";
 import {UserService} from "../../shared/user/user.service";
 import {Router} from "@angular/router";
+import {Page} from "ui/page";
+import {Color} from "color";
+import {View} from "ui/core/view";
+
 
 @Component({
   selector: "my-app",
@@ -9,15 +13,26 @@ import {Router} from "@angular/router";
   templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
 })
-export class LoginPage {
+
+export class LoginPage implements OnInit {
   user: User;
   isLoggingIn = true;
+  @ViewChild("container") container: ElementRef;
+  
+  constructor(private _router: Router, private _userService: UserService, private page: Page) {
 
-  constructor(private _router: Router, private _userService: UserService) {
-	  this.user = new User();
-	  this.user.email = "somo@somombo.com";
-	  this.user.password = "sakala12";
 	}
+  ngOnInit() {   
+	  this.user = new User();
+    //	  this.user.email = "somo@somombo.com";
+    //	  this.user.password = "sakala12";
+	  this.user.email = "user@nativescript.org";
+	  this.user.password = "password";
+    
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = "res://bg_login";
+  }
+  
 	submit() {
 		if (this.isLoggingIn) {
 			this.login();
@@ -44,7 +59,13 @@ export class LoginPage {
 				() => alert("Unfortunately we were unable to create your account.")
 			);
 	}
+  
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+      duration: 200
+    });
   }
 }
